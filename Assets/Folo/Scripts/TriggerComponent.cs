c34ddb1a36d83f4ed8956d64100919633001b510
Amir <abu.amir@mail.ru>
2022-01-23 17:46:01 +0300

    2D Game Kit

---
+++ Assets/Folo/Scripts/TriggerComponent.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class TriggerComponent : MonoBehaviour
+{
+    // public static void Create()
+    // {
+    //     
+    // }
+}
