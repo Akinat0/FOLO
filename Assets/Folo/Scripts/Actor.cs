30fd2b7efef7cf0cf4c35ba2998953bcd9611eaa
Amir <a.abumahadi@playquantum.com>
2022-01-30 23:44:40 +0300

    Interactable graphs

--- Assets/Folo/Scripts/Actor.cs
+++ Assets/Folo/Scripts/Actor.cs
@@ -1,39 +1,46 @@
-﻿
-using Bolt;
+﻿using Bolt;
 using UnityEngine;
 
-[RequireComponent(typeof(StateMachine))]
-public class Actor : MonoBehaviour
+namespace Folo.Scripts
 {
-    [SerializeField] string actorName;
-
-    public string ActorName => !string.IsNullOrEmpty(actorName) ? actorName : gameObject.name;
-
-    public Vector3 Position => transform.position;
-
-    public void SendTriggerEnterEvent(Actor other)
+    [RequireComponent(typeof(StateMachine))]
+    public class Actor : MonoBehaviour
     {
-        SendCustomEvent($"{other.ActorName}_Enter", other);
-    }
+        [SerializeField] string actorName;
+
+        public string ActorName => !string.IsNullOrEmpty(actorName) ? actorName : gameObject.name;
+
+        public Vector3 Position => transform.position;
+
+        public void SendTriggerEnterEvent(Actor other)
+        {
+            SendCustomEvent($"{other.ActorName}_Enter", other);
+        }
     
-    public void SendTriggerStayEvent(Actor other)
-    {
-        SendCustomEvent($"{other.ActorName}_Stay", other);
-    }
+        public void SendTriggerStayEvent(Actor other)
+        {
+            SendCustomEvent($"{other.ActorName}_Stay", other);
+        }
     
-    public void SendTriggerExitEvent(Actor other)
-    {
-        SendCustomEvent($"{other.ActorName}_Exit", other);
-    }
+        public void SendTriggerExitEvent(Actor other)
+        {
+            SendCustomEvent($"{other.ActorName}_Exit", other);
+        }
 
-    public void SendInteractEvent(Actor actor)
-    {
-        SendCustomEvent("Interact", actor);
-    }
+        public void SendInteractEvent(Actor interactor)
+        {
+            SendCustomEvent("Interact", interactor);
+        }
     
-    public void SendCustomEvent(string eventName, params object[] args)
-    {
-        Debug.Log($"Trigger event {eventName} on {ActorName}");
-        CustomEvent.Trigger(gameObject, eventName, args);
+        public void SendSetInteractableEvent(bool interactable)
+        {
+            SendCustomEvent("SetInteractable", interactable);
+        }
+    
+        public void SendCustomEvent(string eventName, params object[] args)
+        {
+            Debug.Log($"Trigger event {eventName} on {ActorName}");
+            CustomEvent.Trigger(gameObject, eventName, args);
+        }
     }
 }
